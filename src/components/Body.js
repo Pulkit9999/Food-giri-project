0671837f06f846f0870ee26b965fa980c2093e9b
Pulkit Acharya <pullkit.9@gmail.com>
2024-07-15 01:04:49 +0530

    Added shimmer effect on button click

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
 import CategoryCard from "./CategoryCard";
+import Shimmer from "./Shimmer";
 import { GET_RES_API_URL } from "./utils/mockData";
 
 const Body = () => {
   const [restaurantCategories, setRestaurantCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
+  const [loadingCategories, setLoadingCategories] = useState(false);
 
-  const fetchData = async () => {
+  const fetchCategories = async () => {
     try {
+      setLoadingCategories(true);
       const data = await fetch(GET_RES_API_URL);
       const json = await data.json();
       setRestaurantCategories(json?.results?.restaurants);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setLoadingCategories(false);
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   const handleButtonClick = () => {
+    if (!showCategories) {
+      fetchCategories();
+    }
     setShowCategories(!showCategories);
   };
 
@@ -28,20 +32,29 @@ const Body = () => {
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={handleButtonClick}>
-          {showCategories ? "Hide Top Rated Restaurants" : "Search Categorywise Restaurants"}
+          {showCategories
+            ? "Hide Top Rated Restaurants"
+            : "Search Categorywise Restaurants"}
         </button>
-        <br/><br/>
-        {showCategories && restaurantCategories.length > 0 && (
-          <div className="category-list">
-            {restaurantCategories.map((category) => (
-              <CategoryCard
-                key={category.text}
-                imageUrl={category.image_url}
-                description={category.description}
-                text={category.text}
-              />
-            ))}
-          </div>
+        <br />
+        <br />
+        {showCategories && (
+          loadingCategories ? (
+            <Shimmer />
+          ) : (
+            restaurantCategories.length > 0 && (
+              <div className="category-list">
+                {restaurantCategories.map((category) => (
+                  <CategoryCard
+                    key={category.text}
+                    imageUrl={category.image_url}
+                    description={category.description}
+                    text={category.text}
+                  />
+                ))}
+              </div>
+            )
+          )
         )}
       </div>
     </div>
